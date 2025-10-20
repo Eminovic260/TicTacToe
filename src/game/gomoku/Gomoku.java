e3b65b1f9569d101e117b302b3b9f31a2d298741
Elric TOURTEL <tourtel.elric@gmail.com>
2025-10-20 19:44:51 +0200

    Inheritance for tic tac toe power4 and gomoku

--- src/game/gomoku/Gomoku.java
+++ src/game/gomoku/Gomoku.java
@@ -1,45 +1,21 @@
-package game.tictactoe;
+package game.gomoku;
+
+import entites.Player;
 import game.Game;
 import ui.View;
-import model.Cell;
-import entites.Player;
 
-public class TicTacToe extends Game {
-    public static final int SIZE = 3;
-    private static final int WIN_LENGTH = 3;
+public class Gomoku extends Game {
+    public static final int SIZE = 15;
+    private static final int WIN_LENGTH = 5;
 
-    public TicTacToe(Player player1, Player player2, View view) {
+    public Gomoku(Player player1, Player player2, View view) {
         super(new Player[]{player1, player2}, view, SIZE, SIZE);
     }
 
     @Override
     public void run() {
-        while (true) {
-            view.display(board);
-            Player currentPlayer = getCurrentPlayer();
-            int[] move = currentPlayer.getMove(board);
-            setOwner(move[0], move[1], currentPlayer);
 
-            if (checkWin(move[0], move[1])) {
-                view.display(board);
-                String symbol = currentPlayer.getRepresentation();
-                view.displayWinner(symbol);
-                break;
-            }
-            if (isBoardFull()){
-                view.display(board);
-                view.displayDraw();
-                break;
-            }
-
-            switchPlayer();
-        }
     }
-
-
-
-
-
 
     @Override
     public boolean checkWin(int myX, int myY){
@@ -76,8 +52,7 @@ public class TicTacToe extends Game {
     }
 
     @Override
-    protected boolean isValidMove(int row, int col) {
-        return isValidCoordinate(row, col) && board[row][col].isEmpty();
+    protected boolean isValidMove(int row, int col){
+        return false;
     }
-
 }
